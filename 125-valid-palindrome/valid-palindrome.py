222ef21d454688916850c663b49e15f77806f74b
DEVISETTI SAI PAVAN KUMAR <devisettisaipavankumar@gmail.com>
2025-08-04 19:12:38 +0530

    Time: 4 ms (90.74%) | Memory: 23.2 MB (16.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        k =""
-        for i in s:
-            if(i.isalpha()):
-                k+=i.lower()
-            elif(i.isdigit()):
-                k+=i
-        
-        if(k == k[::-1]):
-            return True
-        return False
+        joi = ''.join(c.lower() for c in s if c.isalnum())
+        return joi == joi[::-1]
