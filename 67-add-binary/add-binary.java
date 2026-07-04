d57eec0f15cd7743b1c9e62d13ebfccdd9571897
DEVISETTI SAI PAVAN KUMAR <devisettisaipavankumar@gmail.com>
2026-07-04 14:10:14 +0530

    Time: 1 ms (99.93%) | Memory: 43.8 MB (17.58%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,9 +4,9 @@ class Solution {
         int lb = b.length()-1;
         int sum =0;
         int carry =0;
-        String res ="";
+        StringBuilder res = new StringBuilder();
         while(la>=0 || lb>=0 || carry ==1){
-            sum = 0;
+            sum = carry;
             if(la>=0){
                 sum = (a.charAt(la)- '0') + sum;
                 la--;
@@ -15,26 +15,11 @@ class Solution {
                 sum = (b.charAt(lb)- '0') + sum;
                 lb--;
             }
-            if(carry==1){
-                carry =0;
-                sum = sum + 1;
-            }
-            if(sum==0){
-                res = "0"+res;
-            }
-            else if(sum==1){
-                res = "1"+res;
-            }
-            else if(sum==2){
-                res = "0"+res;
-                carry = 1;
-            }
-            else{
-                res = "1" +res;
-                carry = 1;
-            }
+            res.append(sum % 2);
+            carry=sum/2;
+            
 
         }
-        return res;
+        return res.reverse().toString();
     }
 }
